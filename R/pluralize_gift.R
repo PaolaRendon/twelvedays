31faab747f5bc730f88bd2732303bdf1aa4a6dd2
PaolaRendon <68972832+PaolaRendon@users.noreply.github.com>
2020-07-31 20:56:51 -0700

    Add functions

---
+++ R/pluralize_gift.R
@@ -0,0 +1,24 @@
+#' Takes a noun and makes it plural
+#'
+#' @param gift A string or vector of strings
+#'
+#' @return A string or vector of strings with the pluralized words
+#'
+#' @import stringr
+#' @import dplyr
+#' @import glue
+#' @import purrr
+#'
+#' @export
+#'
+
+pluralize_gift <- function(gift){
+
+gift <- gift %>%
+  str_replace("\\b$", "s")
+
+return(gift)
+
+}
+
+pluralize_gift(xmas$Gift.Item)
